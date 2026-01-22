4b253d29f308ba901eb20b3d5921bc58dc56fe25
Arya15302-sketch <Arya.15302@stu.upes.ac.in>
2026-01-22 08:56:15 +0530

    Create carr.java

---
+++ carr.java
@@ -0,0 +1,10 @@
+class car {
+    int modelYear;
+    String modelName;
+    public static void main(String[] args) {
+        car myCar = new car();
+        myCar.modelYear = 1969;
+        myCar.modelName = "Mustang";
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
+    }
+}
