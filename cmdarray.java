1075ddaff4d256f4146a65713d7987f894254b94
Arya15302-sketch <Arya.15302@stu.upes.ac.in>
2026-01-31 08:30:45 +0530

    Add files via upload

---
+++ cmdarray.java
@@ -0,0 +1,8 @@
+class CmdArray {
+    public static void main(String[] args) {
+        System.out.println("Array elements are:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
+    }
+}
