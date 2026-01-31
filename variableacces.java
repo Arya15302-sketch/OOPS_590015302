ba7932e636de7e3dc80b1c128fc1356d7ed07d92
Arya15302-sketch <Arya.15302@stu.upes.ac.in>
2026-01-31 08:41:13 +0530

    Add files via upload

---
+++ variableacces.java
@@ -0,0 +1,8 @@
+class VariableAccess {
+    int x = 50;
+
+    public static void main(String[] args) {
+        VariableAccess obj = new VariableAccess();
+        System.out.println(obj.x);
+    }
+}
