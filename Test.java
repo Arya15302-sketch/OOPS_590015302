d9d7833b4ee42ebd876d93c8fbf308f513a2755c
Arya15302-sketch <Arya.15302@stu.upes.ac.in>
2026-02-04 08:33:24 +0530

    Add classes A and B with constructors

---
+++ Test.java
@@ -0,0 +1,18 @@
+class A {
+    A() {
+        System.out.println("Class A constructor");
+    }
+}
+
+class B extends A {
+    B() {
+        super();   // calls parent class constructor
+        System.out.println("Class B constructor");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        B obj = new B();
+    }
+}
